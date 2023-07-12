32ae8d0fe3d553ffd419b7c52d848cc9bec50f76
Kravchenko, Dmitry2 <d2.kravchenko@a1qa.com>
2023-07-12 11:05:14 +0300

    1

--- src/test/java/tests/SuperTest.java
+++ src/test/java/tests/SuperTest.java
@@ -8,7 +8,7 @@ public class SuperTest {
 	@Test
 	public void someTest() {
 
-
+		System.out.println("Our password in test is: ");
 		System.out.println(System.getProperty("DATA") == null ?
 				"NO DATA <<<"
 				: System.getProperty("DATA")
